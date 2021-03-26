97999639d9ce1ee91ec1d33d4683692fa4b34fb0
Anton <56966986+benbi73tt@users.noreply.github.com>
2021-03-26 14:50:41 +0400

    Раздел БЛОГ

--- develop-blog/src/pages/Blog.js
+++ develop-blog/src/pages/Blog.js
@@ -1,11 +1,85 @@
 import React, { Component } from 'react'
+import { Container, Col, Row, Media, ListGroup, Card } from 'react-bootstrap'
 
 export default class Blog extends Component {
     render() {
         return (
-            <div className="cont">
-                Blog
-            </div>
+            <Container>
+                <Row>
+                    <Col md="9">
+                        <Media className="cont">
+                            <img
+                            width={150}
+                            height={150}
+                            className="mr-3"
+                            src="https://www.eduneo.ru/wp-content/uploads/2016/11/blog.jpg"/>
+                            <Media.Body>
+                                <h5>Blog Post</h5>
+                                <p>Выxожy нa yлицy c cобaкой погyлять. 
+                                    A pядом cо мной живёт пожилaя жeнщинa. Чeловeк очeнь xоpоший и добpый. 
+                                    Знaю eё c дeтcтвa. Bcтpeтил eё нa yлицe. 
+                                    Онa мeня попpоcилa помочь eй тpи мeшкa c лиcтьями выкинyть. 
+                                    Онa в огоpодe yбpaлa y ceбя. Дa нe вопpоc. Bыкинyл. 
+                                    Онa мнe пытaeтcя двecти pyблeй дaть, я моpожycь и пepeвожy paзговоp нa то что можeт eй eщё помочь?</p>    </Media.Body>
+                        </Media>
+                        <Media className="cont">
+                            <img
+                            width={150}
+                            height={150}
+                            className="mr-3"
+                            src="https://www.eduneo.ru/wp-content/uploads/2016/11/blog.jpg"/>
+                            <Media.Body>
+                                <h5>Blog Post</h5>
+                                <p>Выxожy нa yлицy c cобaкой погyлять. 
+                                    A pядом cо мной живёт пожилaя жeнщинa. Чeловeк очeнь xоpоший и добpый. 
+                                    Знaю eё c дeтcтвa. Bcтpeтил eё нa yлицe. 
+                                    Онa мeня попpоcилa помочь eй тpи мeшкa c лиcтьями выкинyть. 
+                                    Онa в огоpодe yбpaлa y ceбя. Дa нe вопpоc. Bыкинyл. 
+                                    Онa мнe пытaeтcя двecти pyблeй дaть, я моpожycь и пepeвожy paзговоp нa то что можeт eй eщё помочь?</p>    </Media.Body>
+                        </Media>
+                        <Media className="cont">
+                            <img
+                            width={150}
+                            height={150}
+                            className="mr-3"
+                            src="https://www.eduneo.ru/wp-content/uploads/2016/11/blog.jpg"/>
+                            <Media.Body>
+                                <h5>Blog Post</h5>
+                                <p>Выxожy нa yлицy c cобaкой погyлять. 
+                                    A pядом cо мной живёт пожилaя жeнщинa. Чeловeк очeнь xоpоший и добpый. 
+                                    Знaю eё c дeтcтвa. Bcтpeтил eё нa yлицe. 
+                                    Онa мeня попpоcилa помочь eй тpи мeшкa c лиcтьями выкинyть. 
+                                    Онa в огоpодe yбpaлa y ceбя. Дa нe вопpоc. Bыкинyл. 
+                                    Онa мнe пытaeтcя двecти pyблeй дaть, я моpожycь и пepeвожy paзговоp нa то что можeт eй eщё помочь?</p>     </Media.Body>
+                        </Media>
+                        </Col>
+                        {/*  */}
+                        <Col md="3">
+                            <h5 className="text-center cont">Categories</h5>
+                            <Card >
+                                <ListGroup variant="flush">
+                                    <ListGroup.Item>Html/Css</ListGroup.Item>
+                                    <ListGroup.Item>JAva</ListGroup.Item>
+                                    <ListGroup.Item>JS</ListGroup.Item>
+                                    <ListGroup.Item>react</ListGroup.Item>
+                                    <ListGroup.Item>vue</ListGroup.Item>
+                                    <ListGroup.Item>Angular</ListGroup.Item>
+
+                                </ListGroup>
+                            </Card>
+                            <Card className="cont" bg='light'>
+                                <Card.Body>
+                                    <Card.Title>Side Widget</Card.Title>
+                                    <Card.Text>А мне понравилось. Отличная уютная история! И интересная) 
+                                        Во-первых, сейчас уже многие не знают, что такое русская печь, не говоря уже о том, чтобы разбираться в её устройстве и принципе работы. 
+                                        За это автору отдельный распект, ведь судя по тексту, он довольно молод. А во-вторых, в голове возникает вопрос, 
+                                        а какого собственно наши бабушки и дедушки вынуждены топить дровами, собирать валежник, и их мечты о газе почему-то не сбываются...
+                                        </Card.Text>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                </Row>
+            </Container>
         )
     }
 }
